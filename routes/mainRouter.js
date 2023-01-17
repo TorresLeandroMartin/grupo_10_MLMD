e51ccf439544af6001a5b3930289ef1eff42d68a
MSJaco <msjacobowicz@gmail.com>
2023-01-16 21:01:05 -0300

    sprint-tres

---
+++ routes/mainRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const mainController = require('../controllers/mainController')
+
+// const path = require('path'); 
+
+router.get('/', mainController.home);
+
+router.get('descriptionPage', mainController.descriptionPage);
+
+router.get('cart', mainController.cart);
+
+router.get('edicion', mainController.edicion);
+
+router.get('nuevoProducto', mainController.nuevoProducto);
+
+router.get('register', mainController.register);
+
+router.get('login', mainController.login);
+
+
+
+module.exports = router;
