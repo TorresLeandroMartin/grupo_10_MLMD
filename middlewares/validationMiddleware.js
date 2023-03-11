2c17f358597b4969981b6471016487e6ce9c24a9
Aymara <aymaraflaschka@gmail.com>
2023-03-11 11:45:29 -0300

    trabajando validaciones

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -1,13 +1,14 @@
 const path = require("path");
 
-const { check } = require("express-validator");
+const { check} = require("express-validator");
 
 const validationMiddleware = [
-  check("username").notEmpty().withMessage("Debes completar este campo"),
-  check("lastname").notEmpty().withMessage("Debes completar este campo"),
-  check("phone").notEmpty().withMessage("Debes completar este campo"),
-  check("email").notEmpty().withMessage("Debes completar este campo").bail().isEmail().withMessage("Direccion de correo invalida"),
-  check("password").notEmpty().withMessage("Debes completar este campo"),
+  check("firstname").isLength({ min: 5, max: 30 }).withMessage("Tenés que escribir tu nombre"),
+  check("lastname").isLength({ min: 5, max: 30 }).withMessage("Tenés que escribir tu apellido"),
+  check("phone").isInt().withMessage("Tenés que escribir tu teléfono"),
+  check("email").notEmpty().withMessage("Tenés que escribir tu correo electrónico").bail()
+  .isEmail().withMessage("Escribi un formato válido de correo electrónico"),
+  check("password").isLength({ min: 8 }).withMessage("Escribi tu contraseña"),
   check("file").custom((value, { req }) => {
     let file = req.file;
     let acceptedExtensions = [".jpg", ".png", ".gif"];
