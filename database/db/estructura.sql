37ad180a0136a0ea80d377136d245ed143254821
MSJaco <msjacobowicz@gmail.com>
2023-03-19 16:12:45 -0300

    DB creada + Models actualizado

--- database/db/estructura.sql
+++ database/db/estructura.sql
@@ -1,1 +1,127 @@
--- Active: 1679242072439@@localhost@8889@estructura
+CREATE DATABASE  IF NOT EXISTS `estructura` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `estructura`;
+-- MariaDB dump 10.19  Distrib 10.4.27-MariaDB, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: estructura
+-- ------------------------------------------------------
+-- Server version	5.7.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Carrito`
+--
+
+DROP TABLE IF EXISTS `Carrito`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Carrito` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cantidad` int(11) NOT NULL,
+  `cupon` int(11) NOT NULL,
+  `producto_precio` int(11) NOT NULL,
+  `precio_total` bigint(20) NOT NULL,
+  `Producto_id` int(10) unsigned NOT NULL,
+  `createdAt` date DEFAULT NULL,
+  `updatedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Producto_id` (`Producto_id`),
+  CONSTRAINT `carrito_ibfk_1` FOREIGN KEY (`Producto_id`) REFERENCES `Producto` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Carrito`
+--
+
+LOCK TABLES `Carrito` WRITE;
+/*!40000 ALTER TABLE `Carrito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Carrito` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Producto`
+--
+
+DROP TABLE IF EXISTS `Producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Producto` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `estilo` text NOT NULL,
+  `nombre` text NOT NULL,
+  `precio` bigint(20) NOT NULL,
+  `talle` text NOT NULL,
+  `categoria` text NOT NULL,
+  `descripcion` text NOT NULL,
+  `color` text NOT NULL,
+  `Carrito_id` int(10) unsigned NOT NULL,
+  `createdAt` date DEFAULT NULL,
+  `updatedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Carrito_id` (`Carrito_id`),
+  CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`Carrito_id`) REFERENCES `Carrito` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Producto`
+--
+
+LOCK TABLES `Producto` WRITE;
+/*!40000 ALTER TABLE `Producto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Producto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Usuario`
+--
+
+DROP TABLE IF EXISTS `Usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Usuario` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `imagen` varbinary(8000) DEFAULT NULL,
+  `nombre` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `categoria` text NOT NULL,
+  `contrasena` varchar(30) NOT NULL,
+  `Carrito_id` int(10) unsigned NOT NULL,
+  `createdAt` date DEFAULT NULL,
+  `updatedAt` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Carrito_id` (`Carrito_id`),
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`Carrito_id`) REFERENCES `Carrito` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Usuario`
+--
+
+LOCK TABLES `Usuario` WRITE;
+/*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-19 16:10:49
